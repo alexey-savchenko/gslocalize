bd86df5a5dcd00c40b3a6c56a9d88a5b8feb6a5e
Alexey Savchenko <alexey.savchenko@gen.tech>
2021-04-26 19:53:45 +0300

    Progress with upload

--- upload.py
+++ upload.py
@@ -4,6 +4,7 @@ import sys
 import auth
 import re
 import model
+import string
 from googleapiclient.discovery import build
 
 configuration_file = open('config.yaml', 'r')
@@ -21,14 +22,21 @@ def main():
   service = build('sheets', 'v4', credentials=creds)
   sheet = service.spreadsheets()
 
-  locales = list(map(lambda v: v.split('.')[0], filter(
-      lambda v: 'lproj' in v, os.listdir(TARGET_FOLDER_PATH))))
+  locales = list(
+      map(
+          lambda v: v.split('.')[0],
+          filter(
+              lambda v: 'Base' not in v and 'lproj' in v,
+              os.listdir(TARGET_FOLDER_PATH)
+          )
+      )
+  )
+
+  locales.sort()
 
   for locale in locales:
-    if locale == 'Base':
-      continue
-
-    targetColumn = locales.index(locale) + 1
+    columnIndex = locales.index(locale)
+    columnLetter = string.ascii_uppercase[1:][columnIndex]
 
     targetLocaleFolder = TARGET_FOLDER_PATH + '/' + locale + ".lproj"
     targetFile = targetLocaleFolder
@@ -38,21 +46,54 @@ def main():
     elif UPLOAD_TARGET == 'LocalizablePlist':
       targetFile += '/InfoPlist.strings'
 
-    fileStrings = []
+    fileStrings = map(
+        lambda v: v.rstrip(),
+        open(targetFile, 'r')
+    )
 
-    try:
-      fileStrings = list(filter(lambda v: '//' not in v and v !=
-                         '', open(targetFile, 'r').read().split('\n')))
-    except:
-      print(targetLocaleFolder + ' does not contain ' + targetFile)
+    # for item in fileStrings:
+    #   if len()
 
-    pattern = r'/(?<=((?<=[\s,.:;\"\']|^)[\"\']))(?:(?=(\\?))\2.)*?(?=\1)/gmu'
+    translations = list(map(
+        lambda v: v[1:len(v) - 2],
+        translations
+    ))
 
-    for item in fileStrings:
-      print(re.search(pattern, item).group())
+    # print(translations)
+    # contents = []
+
+    # for item in fileStrings:
+    #   components = item.split(' = ')
+    #   term = components[0].replace('"', '')
+    #   translation = ''
+    #   # components[1][1:len(components[1]) - 2]
+
+    #   try:
+    #     translation = components[1][1:len(components[1]) - 2]
+    #   except:
+    #     print(item, components)
+
+    # print(term, translation)
+    # print(re.match(pattern, item).group(0))
+  #     rawTranslation = list(map(processRawString, re.match(pattern, item).group(0)))
+  #     if len(rawTranslation) != 2:
+  #       print(rawTranslation)
+  #     contents.append(model.Translation(
+  #         safe_list_get(rawTranslation, 0, ''),
+  #         safe_list_get(rawTranslation, 1, '')
+  #     ))
+
+  #   terms = list(map(lambda v: [v.term], contents))
+  #   translations = list(map(lambda v: [v.translation], contents))
+  #   translationsContainer.append(translations)
+  #   translationsRange = UPLOAD_TARGET + '!' + columnLetter + '2:' + columnLetter
+
+  #   sheet.values().update(
+  #       spreadsheetId=SPREADSHEET_ID, range=translationsRange,
+  #       valueInputOption='RAW', body={'values': translations}).execute()
+
+  # rangeToSet = UPLOAD_TARGET + '!A2:A'
   # sheet.values().update(
-  #     spreadsheetId=SPREADSHEET_ID, range=range_name,
-  #     valueInputOption='RAW', body=body)
-
-
-
+  #     spreadsheetId=SPREADSHEET_ID, range=rangeToSet,
+  #     valueInputOption='RAW', body={'values': terms}).execute()
+main()
